0aa4629ce60e5fe53b8e1fccbcc3df38552300de
bsMaria <bmaria.contact@gmail.com>
2020-05-27 16:06:08 -0300

    Javascript functionality added

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const slideOutMenu = document.getElementById('slide-out-menu');
+const menubtn = document.getElementById('bars-icon');
+const searchBox = document.getElementById('search-box');
+const searchBtn = document.getElementById('search-btn');
+const passwordInput = document.getElementById('password-input');
+const toogleText = document.getElementById('passtext');
+
+
+//SHOW AND HIDE mobile btn - CODE 
+
+menubtn.addEventListener('click', function() {
+    if (slideOutMenu.style.opacity == 0) {
+        slideOutMenu.style.opacity = 1;
+    } else {
+        slideOutMenu.style.opacity = 0;
+    }
+});
+
+// Show search - CODE 
+
+searchBtn.addEventListener('click', function() {
+    if (searchBox.style.opacity == 0) {
+        searchBox.style.opacity = 1;
+    } else {
+        searchBox.style.opacity = 0;
+    }
+});
+
+
+// SHOW PASSWORD - CODE
+toogleText.addEventListener('click', function() {
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text"
+    } else {
+        passwordInput.type = "password";
+    }
+});
